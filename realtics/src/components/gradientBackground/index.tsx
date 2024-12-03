a2edf42f88a81a6b6020973bd0a479b6dff3fa90
Leone Varando <LVARANDO@GMAIL.COM>
2024-12-03 10:45:59 -0600

    Created GradientBackground component

---
+++ realtics/src/components/gradientBackground/index.tsx
@@ -0,0 +1,13 @@
+import './style/index.css';
+/**
+ * Component that creates a reusable gradient background for use on multiple pages.
+ * 
+ * @returns React.JSX.Element
+ */
+export default function GradientBackground(): React.JSX.Element {
+  return (
+    <>
+      <div id="gradientBackground"></div>
+    </>
+  )
+}
